6f787502f36ee95fed79fbd3b0ad7454b61c52ff
JojuGeorge <jojugeorge001@gmail.com>
2019-09-24 19:26:01 +0530

    Cube editor - Execute in edit mode
    CubeEditorInEditMode.cs added to each nodes. Adding functionality to run the script in edit mode. Therefore we can snap the cubes in edit mode with preferred grid size to snap to.

---
+++ Breadth_First_Search/Assets/Scripts/CubeEditorInEditMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+                                                          // The size of cube is currently 10 in x and z
+[ExecuteInEditMode]                                       // This script will be executed in edit mode. No need to go to play mode
+public class CubeEditorInEditMode : MonoBehaviour
+{
+    [SerializeField] private int _gridSize = 10;          // Grid snap units
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+    }
+
+    private void SnapToGrid()
+    {
+        int newX = Mathf.RoundToInt(transform.position.x / _gridSize);
+        int newZ = Mathf.RoundToInt(transform.position.z / _gridSize);
+
+        transform.position = new Vector3(newX * _gridSize, 0f, newZ * _gridSize);
+    }
+}
