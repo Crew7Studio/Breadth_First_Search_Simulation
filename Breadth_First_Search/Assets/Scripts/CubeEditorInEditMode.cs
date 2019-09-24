9c9e4e48f95c9e9256ee3d5166cd9e6809efc86e
JojuGeorge <jojugeorge001@gmail.com>
2019-09-24 20:00:59 +0530

    Added Node.cs
    CubeEditorInEditMode.cs references position and grid size from Node.cs to snap to grid

--- Breadth_First_Search/Assets/Scripts/CubeEditorInEditMode.cs
+++ Breadth_First_Search/Assets/Scripts/CubeEditorInEditMode.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+                
+                                                          // Script for snapping the nodes / cube in the editor
                                                           // The size of cube is currently 10 in x and z
 [ExecuteInEditMode]                                       // This script will be executed in edit mode. No need to go to play mode
+[RequireComponent(typeof(Node))]
 public class CubeEditorInEditMode : MonoBehaviour
 {
-    [SerializeField] private int _gridSize = 10;          // Grid snap units
+    private Node _node;
+    private int _gridSize;                                // Grid snap units; Gets initialized from Node.cs
+
+
+
+    private void OnEnable()
+    {
+        _node = GetComponent<Node>();                     // Need to get node here else wont work
+    }
+
+
 
     void Start()
     {
-        
+        _gridSize = _node.GridSize;
     }
+
+
 
     void Update()
     {
@@ -19,9 +34,7 @@ public class CubeEditorInEditMode : MonoBehaviour
 
     private void SnapToGrid()
     {
-        int newX = Mathf.RoundToInt(transform.position.x / _gridSize);
-        int newZ = Mathf.RoundToInt(transform.position.z / _gridSize);
-
-        transform.position = new Vector3(newX * _gridSize, 0f, newZ * _gridSize);
+        // For snapping to grid
+        transform.position = new Vector3(_node.GetPos().x * _gridSize, 0f, _node.GetPos().y * _gridSize);
     }
 }
---
+++ Breadth_First_Search/Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // Singleton
+    private static Node _instance;
+    public static Node Instance { get { return _instance; } }
+
+    [SerializeField] private int _gridSize = 11;                              // Grid snap units
+    public int GridSize { get { return _gridSize; } }
+
+
+
+    private void OnEnable()
+    {
+        if (_instance == null) { _instance = this; }         // Use this while using Singleton
+    }
+
+
+    // Get current position; Used in CubeEditorInEditMode.cs for snapping to grid
+    public Vector2Int GetPos()
+    {
+        return new Vector2Int(Mathf.RoundToInt(transform.position.x / _gridSize), 
+                              Mathf.RoundToInt(transform.position.z / _gridSize));
+    }
+}
